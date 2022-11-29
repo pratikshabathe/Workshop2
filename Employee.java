78c9c13d67282c2c6e8bae70cdda465ade24c054
pratikshabathe <pratikshabathe11@gmail.com>
2022-11-29 15:40:19 +0530

    Added: workshop program

---
+++ Employee.java
@@ -0,0 +1,29 @@
+package com.blz.employee;
+
+import java.util.Scanner;
+
+public class Employee {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		String name = "abc";
+		int age = 24;
+		int phoneNum = 1234567890;
+		String state = "maharashtra";
+		String city = "pune";
+		String deparment = "java devloper";
+	
+		System.out.println("enter employee name: " +name);
+		System.out.println("enter employee age: " +age);
+		System.out.println("enter employee phone number: " +phoneNum);
+		System.out.println("enter employee city: " +city);
+		System.out.println("enter employee State: " +state);
+		System.out.println("enter employee deparment: " +deparment);
+	}
+}
+	
+		
+		 
+		
+
